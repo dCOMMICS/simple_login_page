58a21e64790c00d1c6dfca1894f9c8eefbabfb51
dCOMMICS <sanwurld@gmail.com>
2024-03-04 19:49:46 +0300

    initial commit

--- app/src/main/java/com/example/simplelogin/MainActivity.kt
+++ app/src/main/java/com/example/simplelogin/MainActivity.kt
@@ -1,11 +1,57 @@
+//package com.example.simplelogin
+//
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import android.util.Log
+//import android.widget.Button
+//import android.widget.EditText
+//
+//class MainActivity : AppCompatActivity() {
+//
+//    lateinit var usernameInput: EditText
+//    lateinit var passwordInput: EditText
+//    lateinit var loginBtn: Button
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        usernameInput = findViewById(R.id.username_input)
+//        passwordInput = findViewById(R.id.userpassword_input)
+//        loginBtn = findViewById(R.id.login_btn)
+//
+//        loginBtn.setOnClickListener {
+//            val username = usernameInput.text.toString()
+//            val password = passwordInput.text.toString()
+//            Log.i(tag: "Test Credentials", msg: "Username : $username and Password : $password")
+//        }
+//    }
+//}
+
 package com.example.simplelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var usernameInput: EditText
+    private lateinit var passwordInput: EditText
+    private lateinit var loginBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.userpassword_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Test Credentials", "Username: $username and Password: $password")
+        }
     }
 }
